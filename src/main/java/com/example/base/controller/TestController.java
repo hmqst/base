49af9a97bf9e461a45cc328859df4ccd09a8beb9
yun00feng <846130925@qq.com>
2021-08-04 09:44:57 +0800

    添加邮件带附件（数据库备份）发送

--- src/main/java/com/example/base/controller/TestController.java
+++ src/main/java/com/example/base/controller/TestController.java
@@ -11,13 +11,27 @@ import com.example.base.utils.ResultBean;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -32,8 +46,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("test")
 @Api(tags = "测试相关")
+@Slf4j
 public class TestController {
-
 
     @Resource
     private RabbitTemplate rabbitTemplate;
@@ -48,7 +62,22 @@ public class TestController {
     private ObjectMapper objectMapper;
 
     @Autowired
-    JavaMailSender javaMailSender;
+    JavaMailSender mailSender;
+
+    @Value("${spring.datasource.url}")
+    String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.mail.username}")
+    String fromEmailAddress;
+
+    @Value("${spring.mail.to}")
+    String[] toEmailAddress;
 
     @ApiOperation("Jackson自定义全局序列化测试")
     @RequestMapping("jackson")
@@ -137,16 +166,113 @@ public class TestController {
     }
 
     @ApiOperation("邮件发送测试")
-    @RequestMapping("sendMail")
-    public String sendMail() {
+    @RequestMapping("sendSimpleMail")
+    public String sendSimpleMail() {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setSubject("测试邮件");
-        mailMessage.setFrom("发送方地址");
-        mailMessage.setTo("接收方地址");
+        mailMessage.setFrom(fromEmailAddress);
+        mailMessage.setTo(toEmailAddress);
         mailMessage.setText("测试内容");
         mailMessage.setSentDate(new Date());
-        javaMailSender.send(mailMessage);
+        mailSender.send(mailMessage);
         return "发送成功";
+    }
+
+    @ApiOperation("数据库备份并邮件发送测试")
+    @RequestMapping("sendFileMail")
+    public String sendFileMail() {
+        Instant startBackup = Instant.now();
+        Process process = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            // 获取项目根目录
+            ApplicationHome home = new ApplicationHome();
+            // 组织命令并执行 win 平台运行需要 cmd /c
+            // mysqldump -h127.0.0.1 -uroot -p123456 --default-character-set=utf8 local_test > backupfile.sql
+            // mysql -h127.0.0.1 -uroot -p123456 --default-character-set=utf8 local_test < backupfile.sql
+            // docker exec -it root mysqldump -uroot -p123456 -P3306 --default-character-set=utf8 local_test > backupfile.sql
+            // docker exec -it root mysqldump -uroot -p123456 -P3306 --default-character-set=utf8 local_test < backupfile.sql
+            // jdbc:mysql://172.10.8.239:3306/cihong?
+            String url = datasourceUrl.substring(datasourceUrl.indexOf("//") + 2, datasourceUrl.indexOf("?"));
+            String[] split = url.split("/");
+            String[] ipAndPort = split[0].split(":");
+            StringBuilder command = new StringBuilder();
+            if (System.getProperty("os.name").toLowerCase().contains("win")){
+                command.append("cmd /c ");
+            }
+            command.append("mysqldump -h").append(ipAndPort[0])
+                    .append(" -u").append(username)
+                    .append(" -p").append(password)
+                    .append(" -P").append(ipAndPort[1])
+                    .append(" --default-character-set=utf8 ").append(split[1]).append(" > backupfile.sql");
+            process = Runtime.getRuntime().exec(command.toString(), new String[]{}, home.getDir());
+            // 获取返回结果
+            inputStreamReader = new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while (null != (line = bufferedReader.readLine())) {
+                if (builder.length() > 0){
+                    builder.append("  |  ");
+                }
+                builder.append(line);
+            }
+            int exit = process.waitFor();
+            // 邮件正文
+            String context;
+            File file = null;
+            if (exit == 0) {
+                context = "完成备份时间：" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                // 获取备份好的sql文件
+                file = new File(home.toString() + File.separator + "backupfile.sql");
+            } else {
+                context = builder.length() > 0 ? builder.toString() : "备份失败，请联系管理员修复。";
+            }
+            // 发送邮件
+            Instant startSendMail = Instant.now();
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(fromEmailAddress);
+            helper.setTo(toEmailAddress);
+            helper.setSubject("数据库备份文件定时推送");
+            helper.setText(context, true);
+            if (file != null && file.exists()){
+                // 添加附件
+                FileSystemResource fileSystemResource = new FileSystemResource(file);
+                helper.addAttachment("backupfile.sql", fileSystemResource);
+            }
+            mailSender.send(message);
+            Instant endSendMail = Instant.now();
+            log.info("===============【数据库SQL备份成功】================");
+            log.info("执行命令：" + command);
+            log.info("执行结果：" + context);
+            log.info("邮件发送方：" + fromEmailAddress);
+            log.info("邮件接收方：：" + String.join(" | ", toEmailAddress));
+            log.info("备份用时：" + Duration.between(startBackup, startSendMail).toMillis());
+            log.info("发送邮件用时：" + Duration.between(startBackup, endSendMail).toMillis());
+            log.info("===============【数据库SQL备份成功】================");
+            return "发送成功";
+        } catch (Exception e) {
+            log.error("===============【数据库SQL备份失败】================");
+            e.printStackTrace();
+            log.error("===============【数据库SQL备份失败】================");
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (inputStreamReader != null) {
+                    inputStreamReader.close();
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        return "发送失败";
     }
 
 }
