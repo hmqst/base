f9d9aaa715de8248d50a7bb02f53f364b252badf
yun00feng <846130925@qq.com>
2021-08-10 14:10:45 +0800

    删除无用DateUtil

--- src/main/java/com/example/base/mapper/CarMapper.java
+++ src/main/java/com/example/base/mapper/CarMapper.java
@@ -3,7 +3,6 @@ package com.example.base.mapper;
 import com.example.base.dto.CarDTO;
 import com.example.base.enums.TypeEnum;
 import com.example.base.po.CarPO;
-import com.example.base.utils.DataUtil;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,7 +12,7 @@ import org.mapstruct.Mapping;
  * @Description
  * @date 2021-03-18 15:38
  */
-@Mapper(componentModel = "spring", imports = {TypeEnum.class, DataUtil.class, Math.class})
+@Mapper(componentModel = "spring", imports = {TypeEnum.class, Math.class})
 public interface CarMapper {
     @Mapping(target = "name", ignore = true)
     @Mapping(source = "date", target = "date", dateFormat = "yyyy-MM-dd HH:mm:ss")
@@ -25,6 +24,5 @@ public interface CarMapper {
 
     @Mapping(target = "number", expression = "java(Math.round(carDto.getCount()))")
     @Mapping(target = "type", expression = "java(TypeEnum.valueOfByDisplay(carDto.getType()))")
-    @Mapping(target = "date", expression = "java(DataUtil.getDateToDaySec(carDto.getDate()))")
     CarPO carDtoToCar(CarDTO carDto);
 }
